b0b6609cf18b4b89d13ee6636ae163b2b17ea047
Yeferson <yaguilar@unal.edu.co>
2020-11-20 16:09:32 -0500

    ingresar

---
+++ src/main/java/Nuevo/ingresarControlador.java
@@ -0,0 +1,32 @@
+package Nuevo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class ingresarControlador {
+    @FXML
+    public TextField usuario;
+    @FXML
+    public TextField contraseña;
+    @FXML
+    public Label mensajeUsuario;
+    @FXML
+    public Label mensajeContraseña;
+    @FXML
+    public void iniciar(ActionEvent event)throws IOException {
+        String  usu = "";
+        if (usuario.getText().trim().equals("")){
+            mensajeUsuario.setText("El campo no puede quedar vacio");
+            return;
+        }
+        if (contraseña.getText().trim().equals("")){
+            mensajeContraseña.setText("El campo no puede quedar vacio");
+            return;
+        }
+
+    }
+}
--- src/main/java/Nuevo/menuPrincipalControlador.java
+++ src/main/java/Nuevo/menuPrincipalControlador.java
@@ -1,4 +1,17 @@
 package Nuevo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class menuPrincipalControlador {
+    @FXML
+    public void ingresar (ActionEvent event) throws IOException {
+        App.setRoot("AgregarUsuario");
+    }
+    @FXML
+    public void registrarse (ActionEvent event) throws IOException {
+        App.setRoot("AgregarUsuario");
+    }
 }
