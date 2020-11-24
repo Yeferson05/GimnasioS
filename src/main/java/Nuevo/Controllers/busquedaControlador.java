20d74821f96af13f5ddcb33bf811ea6ec82df68e
Yeferson <yaguilar@unal.edu.co>
2020-11-24 11:45:54 -0500

    buscar(principio(solo nombre sede))

--- src/main/java/Nuevo/Controllers/busquedaControlador.java
+++ src/main/java/Nuevo/Controllers/busquedaControlador.java
@@ -1,32 +1,89 @@
 package Nuevo.Controllers;
 
 
+import Nuevo.App;
+import Nuevo.Sede;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class busquedaControlador implements Initializable {
     @FXML
+    public ListView ListView;
+    @FXML
     public ChoiceBox entidad;
     @FXML
     public ChoiceBox atributo;
-
+    @FXML
+    public TextField ingresar;
+    @FXML
+    public Label mensajeG;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ArrayList<String> op = new ArrayList<>();
         op.add("Sede");
         op.add("Zona");
         op.add("Curso");
-        entidad.setItems(FXCollections.observableList(op));
-        ArrayList<String> atr = new ArrayList<>();
-        atr.add("Nombre");
-        atr.add("Direccion");
-        atr.add("Ciudad");
-        atributo.setItems(FXCollections.observableList(atr));
+        entidad.setItems(FXCollections.observableList(new ArrayList<>(op)));
+    }
+    @FXML
+    public void validar (ActionEvent event)throws IOException {
+        String enti="";
+        //  String.valueOf(atributo.getValue()).equals("null")||
+        // || ingresar.getText().trim().equals("")
+        if (String.valueOf(entidad.getValue()).equals("null") ) {
+            mensajeG.setText("ERROR: Seleccione una entidad");
+            return;
+        }
+        enti = String.valueOf(entidad.getValue());
+        if (enti.equals("Sede")){
+            ArrayList<String> atr = new ArrayList<>();
+            atr.add("Nombre");
+            atr.add("Direccion");
+            atr.add("Ciudad");
+            atributo.setItems(FXCollections.observableList(atr));
+        }else if (enti.equals("Zona")){
+            ArrayList<String> zo = new ArrayList<>();
+            zo.add("Id");
+            zo.add("Zona");
+            atributo.setItems(FXCollections.observableList(zo));
+
+        }
+    }
+    @FXML
+    public void buscar(ActionEvent actionEvent) throws IOException {
+        String atri="";
+        if (String.valueOf(atributo.getValue()).equals("null") || ingresar.getText().trim().equals("")) {
+            mensajeG.setText("ERROR: Existen campos vacios.");
+            return;
+        }
+        atri = String.valueOf(atributo.getValue());
+        String ing = ingresar.getText().trim();
+        if (atri.equals("Nombre")){
+            int cont =0;
+            for (Sede sede1 : Sede.sedes) {
+                if (sede1.nombre.equals(ing)) {
+                    ListView.getItems().add(sede1);
+                }else{
+                    cont++;
+                }
+            }if (cont==0){
+                mensajeG.setText("No se encuentra la sede");
+            }
+        }
+    }
+    @FXML
+    public void volver(ActionEvent actionEvent) throws IOException {
+        App.setRoot("menuPrincipal");
     }
 }
