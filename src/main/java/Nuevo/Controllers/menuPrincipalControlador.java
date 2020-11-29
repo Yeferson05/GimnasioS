b71d0593039ffa03d1e7555c9670b2fe1083cdef
Juan Pablo <juanpa6427@gmail.com>
2020-11-29 17:59:52 -0500

    Auto stash before merge of "main" and "origin/main"
    iniciosesion

--- src/main/java/Nuevo/Controllers/menuPrincipalControlador.java
+++ src/main/java/Nuevo/Controllers/menuPrincipalControlador.java
@@ -1,18 +1,25 @@
 package Nuevo.Controllers;
 
 import Nuevo.App;
+import Nuevo.saludoIngreso;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class menuPrincipalControlador {
+public class menuPrincipalControlador implements Initializable{
+    @FXML
+    public Label saludoin;
 
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        saludoin.setText("Bienvenido "+saludoIngreso.saludo);
+    }
 
     @FXML
     public ImageView ImageView;
