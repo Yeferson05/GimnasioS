d153a215d97c0919b2d305d0468672d68e50d678
Kevin Quiñones <kquinones107@unal.edu.co>
2020-11-30 00:51:14 -0500

    retoques

--- src/main/java/Nuevo/Controllers/agregarCursoASedeControlador.java
+++ src/main/java/Nuevo/Controllers/agregarCursoASedeControlador.java
@@ -1,4 +1,15 @@
 package Nuevo.Controllers;
 
+import Nuevo.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class agregarCursoASedeControlador {
+
+    @FXML
+    public void volver(ActionEvent actionEvent) throws IOException {
+        App.setRoot("sede");
+    }
 }
